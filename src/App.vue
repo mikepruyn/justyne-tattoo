cc2038816ac439fb4ad20da01064bb835a6edd8e
= <mikepruyn8@gmail.com>
2022-02-06 13:58:20 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <home/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
+@font-face {
+  font-family: "Prince Valient";
+  src: local("Prince Valient"),
+   url(./assets/fonts/prince-valiant/Prince-Valiant.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Baldur";
+  src: local("Baldur"),
+   url(./assets/fonts/baldur/Baldur.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Cardinal";
+  src: local("Cardinal"),
+   url(./assets/fonts/cardinal/Cardinal.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Teutonic";
+  src: local("Teutonic"),
+   url(./assets/fonts/teutonic/teutonic1.ttf) format("truetype");
+}
+
+html {
+  cursor: url('./assets/imgs/wand1.png'), auto;
+  overflow: show;
+}
+body {
+  background-color: #e1e5cc;
+}
+button {
+  cursor: inherit;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Cardinal", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  font-size: 64px;
+  font-weight: normal;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <button>Click Mee</button>
+      <H1>Justyne gives u a tattoo? 👉👈</H1>
+      <img src="../assets/imgs/heart-wings.jpg">
+
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+
+</style>
