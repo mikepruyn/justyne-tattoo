4fcfb46ca9df58f8fabf322f588920d4ceb69b14
= <mikepruyn8@gmail.com>
2022-08-15 16:41:39 -0400

    added basic lightbox

--- src/components/WorkPage.vue
+++ src/components/WorkPage.vue
@@ -2,9 +2,13 @@
   <div>
     <h1>Work</h1>
     <div class="imageList">
-        <div v-for="image in this.images" :key="image">
+        <div v-for="image in this.images" :key="image" @click="openLightbox(image)">
             <img :src="image" class="flashImage"/>
         </div>
+    </div>
+    <div v-if="showLightbox" class='lightbox'>
+      <img :src="selectedImage" alt=""/>
+      <button @click="showLightbox=false;">Close</button>
     </div>
   </div>
 </template>
@@ -16,11 +20,19 @@ export default {
     },
     data() {
       return {
-        images: []
+        images: [],
+        selectedImage: '',
+        showLightbox: false
       }
     },
     async mounted() {
       this.images = await getImages('work');
+    },
+    methods: {
+      openLightbox(image) {
+        this.selectedImage = image;
+        this.showLightbox = true;
+      }
     }
 }
 </script>
@@ -36,5 +48,19 @@ export default {
   object-fit: cover;
   width: 30vw;
   height: 30vw;
+}
+.lightbox {
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  top: 0vh;
+  left: 0vw;
+  background-color: bisque;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  row-gap: 20px;
 }
 </style>
